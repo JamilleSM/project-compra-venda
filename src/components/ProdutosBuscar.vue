9f6d1e78239e5e83a28952d9941ae7c7c67a729f
JamilleSM <santosjamille998@gmail.com>
2024-01-17 15:14:41 -0300

    feat: componente de busca

--- src/components/ProdutosBuscar.vue
+++ src/components/ProdutosBuscar.vue
@@ -1,7 +1,13 @@
 <template>
   <section>
     <form action="">
-      <input id="busca" name="busca" type="text" v-model="busca" />
+      <input
+        id="busca"
+        name="busca"
+        type="text"
+        v-model="busca"
+        placeholder="Buscar produto"
+      />
       <input
         id="lupa"
         type="submit"
@@ -26,3 +32,35 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+form {
+  max-width: 600px;
+  margin: 30px auto 50px auto;
+  position: relative;
+}
+
+#busca {
+  width: 100%;
+  padding: 20px;
+  border: none;
+}
+
+#busca:focus,
+#busca:hover {
+  transform: scale(1.04);
+}
+
+#lupa {
+  width: 62px;
+  height: 62px;
+  background: url("../assets/search.svg") no-repeat center;
+  text-indent: -150px;
+  border: none;
+  cursor: pointer;
+  position: absolute;
+  top: 0;
+  right: 0;
+  box-shadow: none;
+}
+</style>
